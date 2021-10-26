7f10f9aaec87c7acc39ba9957859ac0ae58a7eb5
eulalie <emoreau@nvl.fr>
2021-10-26 15:13:29 +0200

    add fixtures

---
+++ src/DataFixtures/PropertyFixture.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Property;
+use App\Service\PropertyHeatHelper;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class PropertyFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 100; $i++) {
+            $property = new Property();
+            $property
+                ->setTitle($faker->sentence(3))
+                ->setDescription($faker->realText(200))
+                ->setSurface($faker->numberBetween(20, 250))
+                ->setRooms($faker->numberBetween(2, 10))
+                ->setBedrooms($faker->numberBetween(1, 6))
+                ->setFloor($faker->numberBetween(0, 5))
+                ->setPrice($faker->numberBetween(30000, 450000))
+                ->setHeat($faker->randomElement(['gaz', 'electrique', 'bois']))
+                ->setCity($faker->city)
+                ->setAddress($faker->address)
+                ->setPostalCode($faker->postcode)
+                ->setSold(false)
+            ;
+            $manager->persist($property);
+        }
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
