63516de1ebe73a3607c7d19ae8b6429365703a1e
eulalie <emoreau@nvl.fr>
2021-10-26 16:12:43 +0200

    wip pagination

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,9 +32,13 @@ class PropertyController extends AbstractController
      * @Route("/acheter", name="PROPERTIES")
      * @return Response
      */
-    public function buy(): Response
+    public function properties(PaginatorInterface $paginator, Request $request): Response
     {
-        $properties = $this->propertyRepository->findAllVisible();
+        $properties = $paginator->paginate(
+            $this->propertyRepository->findAllVisibleQuery(),
+            $request->query->getInt('page', 1),
+            12
+        );
 
         return $this->render('default/properties.html.twig', [
             'properties' => $properties,
@@ -40,21 +46,21 @@ class PropertyController extends AbstractController
     }
 
     /**
-     * @Route("/acheter/{slug}-{id}", name="PROPERTY_SHOW", requirements={"slug": "[a-z0-9\-]*"})
+     * @Route("/acheter/{slug}-{id}", name="PROPERTY", requirements={"slug": "[a-z0-9\-]*"})
      * @return Response
      */
-    public function show($slug, $id): Response
+    public function property($slug, $id): Response
     {
         $property = $this->propertyRepository->find($id);
 
         if ($slug !== $property->getSlug()) {
-            return $this->redirectToRoute('PROPERTY_SHOW', [
+            return $this->redirectToRoute('PROPERTY', [
                 'slug' => $property->getSlug(),
                 'id'   => $id
             ]);
         }
 
-        return $this->render('default/property_show.html.twig', [
+        return $this->render('default/property.html.twig', [
             'property' => $property
         ]);
     }
