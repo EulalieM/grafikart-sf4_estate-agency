fd36d7ab74fbbadacba39032e8d4bae838b2d863
Eulalie <eulalie.m16000@gmail.com>
2022-03-01 09:15:16 +0000

    not important

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,17 @@
-// import Places from "places.js"
-//
-// let inputAddress = document.querySelector('#property_address')
-// if (inputAddress !== null) {
-//     let place = Places({
-//         container: inputAddress
-//     })
-//     place.on('change', e => {
-//         document.querySelector('#property_city').value = e.suggestion.city
-//         document.querySelector('#property_postal_code').value = e.suggestion.postcode
-//         document.querySelector('#property_lat').value = e.suggestion.latlng.lat
-//         document.querySelector('#property_lng').value = e.suggestion.latlng.lng
-//     })
-// }
+import Places from "../../node_modules/places.js"
+
+let inputAddress = document.querySelector('#property_address')
+if (inputAddress !== null) {
+    let place = Places({
+        container: document.querySelector('#property_address')
+    })
+    place.on('change', e => {
+        document.querySelector('#property_city').value = e.suggestion.city
+        document.querySelector('#property_postal_code').value = e.suggestion.postcode
+        document.querySelector('#property_lat').value = e.suggestion.latlng.lat
+        document.querySelector('#property_lng').value = e.suggestion.latlng.lng
+    })
+}
 
 // Suppression images
 document.querySelectorAll('[data-delete]').forEach(a => {
